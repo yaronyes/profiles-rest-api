0adb4412d6484ff1ab1168cf55e12492893b4e4d
yaronyes <yaronyes@hotmail.com>
2020-04-29 12:30:36 +0300

    Enable Django admin for user_progiles model

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -7,23 +7,22 @@ from django.contrib.auth.models import AbstractBaseUser, \
 class UserPorfileManager(BaseUserManager):
     """manager for user profiles"""
 
-    def create_user(self, email, name, password=None):
-        """creating a new user profile"""
+    def create_user(self, email, password=None, **extra_fields):
+        """creates and save a new user"""
         if not email:
-            raise ValueError("users must have an email address")
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+            raise ValueError('users must have email address')
+        
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, name, password):
-        """create a new super user"""
-        user = self.create_user(email, name, password)
-        user.is_superuser = True
+    def create_superuser(self, email, password):
+        """creates and save a new super user"""
+        user = self.create_user(email, password)
         user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
@@ -34,12 +33,11 @@ class UserPorfile(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=254, unique=True)
     name = models.CharField(max_length=254)
     is_active = models.BooleanField(default=True)
-    is_stuff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserPorfileManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIERD_FIELDS = ['name']
+    USERNAME_FIELD = 'email'    
 
     def get_full_name(self):
         """retrieve full name of user"""
