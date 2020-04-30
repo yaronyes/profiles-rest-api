e8cf03ac0a6f64fdbaabfb73e6697ed96ba2a2cd
yaronyes <yaronyes@hotmail.com>
2020-04-30 17:21:19 +0300

    env

--- profiles_project/profiles_project/settings.py
+++ profiles_project/profiles_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v9hu+2=)!s#s9176uzg_*x2=wtpjq7*)2a04kp+-3qunre)yvs'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
