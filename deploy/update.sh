a48960f5da1db52d97cfb669558f7d569187788f
yaronyes <yaronyes@hotmail.com>
2020-05-01 10:10:10 +0300

    fixed typo in update script

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,7 +7,7 @@ PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 git pull
 cd $PROJECT_BASE_PATH
 sudo docker-compose -f docker-compose.prod.yml build
-sudu docker-compose -f docker-compose.prod.yml run --rm profiles_project sh -c "python manage.py migrate --noinput"
+sudo docker-compose -f docker-compose.prod.yml run --rm profiles_project sh -c "python manage.py migrate --noinput"
 sudo docker-compose -f docker-compose.prod.yml run --rm profiles_project sh -c "python manage.py collectstatic --no-input --clear"
 sudo supervisorctl restart profiles_api
 
